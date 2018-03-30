42ff13719756c1d58c9bd9ca961b433fad37393e
Matthew Goggin <matthewgoggin@Matthews-MacBook-Pro.local>
2018-03-30 13:51:06 -0400

    JS update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,18 @@
-var city;
-var state;
 
-city = $("city").val().trim();
+function getWeather() {
+    var city = $("#city").val().trim();
+    var state = $("#state").val().trim();;
+    var weatherURL = "http://api.wunderground.com/api/22c4d183e8562c2d/conditions/q/" + state + '/' + city + '.json';
 
-state = $("state").val().trim();
-
+    $("#submit").click(function () {
+        $.ajax({
+            method: "GET",
+            url: weatherURL
+        }).then(function (response) {
+            $('#weather-display').append(response.display_location.full);
+            $('#weather-display').append(response.temp_f);
+            $('#weather-display').append(response.weather);
+            $('#weather-display').append(response.icon_url);
+        })
+    })
+}
