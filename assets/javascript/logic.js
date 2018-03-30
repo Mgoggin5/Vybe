4b7ad062134e11b5facc9b6bb42f35db8469820a
unknown <mail4khusanboy@gmail.com>
2018-03-30 12:42:43 -0400

    added two more jumbotrons to html file

---
+++ assets/javascript/logic.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+$("#display-weather").hide();
+$("#display-playlist").hide();
+
+$("#add-city").click(function(){
+    $("#display-submit").hide();
+    $("#display-weather").show();
+    $("#display-playlist").show();    
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
