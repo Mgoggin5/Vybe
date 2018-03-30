8b34c13f7e8e3fa83391469ca96f2e7bcb1d163f
Alex Scarlett <alex.scarlett91@gmail.com>
2018-03-30 16:07:56 -0400

    Got link for user to sign in to spotify and redirect them back to app, have button that populates a iframe with a playlist

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,29 +2,58 @@ $(document).ready(function() {
   var timeoutId = 0;
   var token;
 
-  function authorizeApp() {
+  function prepAuthorize() {
     //Grab the hidden elements containing the values of the id and secret
     var a = $('#a').val();
     var b = $('#b').val();
 
     //if the elements exist then save their values in local storage
-    if (a && b) {
+    if (!localStorage.getItem('auth_req_url')) {
       //Use the id and secret to create the base64 string necessary to
       //authenticate
-      localStorage.setItem('auth_creds', btoa(a + ':' + b));
+      var requestObject =
+        'https://accounts.spotify.com/authorize?' +
+        $.param({
+          client_id: a,
+          response_type: 'code',
+          redirect_uri: 'http://localhost:8080',
+          scopes: 'user-read-private user-read-email'
+        });
 
-      //Remove the elements from the page
-      $('#a').remove();
-      $('#b').remove();
+      localStorage.setItem('auth_req_url', requestObject);
+      localStorage.setItem('auth_creds', btoa(a + ':' + b));
     }
 
-    //Client Credentials Flow Ajax Request
+    $('#loginLink').attr('href', localStorage.getItem('auth_req_url'));
+
+    //Remove the elements from the page
+    $('#a').remove();
+    $('#b').remove();
+
+    getToken();
+  }
+
+  function getToken() {
+    var params = new URLSearchParams(location.search.slice(1));
+
+    if (params.get('code')) {
+      var code = params.get('code');
+
+      localStorage.setItem('s_auth_code', code);
+
+      refreshToken();
+    }
+  }
+
+  function refreshToken() {
     $.ajax({
       method: 'POST',
       url:
         'https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token',
       data: {
-        grant_type: 'client_credentials'
+        grant_type: 'authorization_code',
+        code: localStorage.getItem('s_auth_code'),
+        redirect_uri: 'http://localhost:8080'
       },
       headers: {
         Authorization: 'Basic ' + localStorage.getItem('auth_creds')
@@ -32,61 +61,13 @@ $(document).ready(function() {
     }).then(function(response) {
       //Save the token returned into local storage
       localStorage.setItem('token', response.access_token);
-
-      clearTimeout(timeoutId);
-
-      //5 seconds before the token expiration is up, call authorizeApp
-      //The token has a 1 hour expiration
-      timeoutId = setTimeout(authorizeApp, response.expires_in * 1000 - 5000);
+      localStorage.setItem('refresh_token', response.refresh_token);
     });
   }
 
-  //Just a function written to test the artist search after I've authenticated
-  //and set the token.
-  function artistSearch(artist) {
-    if (!localStorage.getItem('token')) {
-      authorizeApp();
-    } else {
-      $.ajax({
-        url: 'https://api.spotify.com/v1/search?type=artist&q=' + artist,
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
-      }).then(function(response) {
-        console.log(response);
-      });
-    }
-  }
-
-  //Click function to execute the artist search based on user input
-  $('#search').click(function() {
-    event.preventDefault();
-    var name = $('#artist')
-      .val()
-      .trim();
-
-    if (name === '') {
-      return;
-    }
-
-    artistSearch(name);
-  });
-
-  //When the page load, execute the authorizeApp function
-  authorizeApp();
-
-  // function millisToMinutesAndSeconds(millis) {
-  //   var minutes = Math.floor(millis / 60000);
-  //   var seconds = ((millis % 60000) / 1000).toFixed(0);
-  //   return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-  // }
-
-  // millisToMinutesAndSeconds(298999); // "4:59"
-  // millisToMinutesAndSeconds(60999); // "1:01"
-
   function displayPlaylist() {
     if (!localStorage.getItem('token')) {
-      authorizeApp();
+      getToken();
     } else {
       $.ajax({
         method: 'GET',
@@ -97,40 +78,23 @@ $(document).ready(function() {
         }
       }).then(function(response) {
         console.log(response);
-        // $('#playlist-display').append(response.tracks.items);
-        function millisToMinutesAndSeconds(millis) {
-          var minutes = Math.floor(millis / 60000);
-          var seconds = ((millis % 60000) / 1000).toFixed(0);
-          return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-        }
 
-        var tracksArray = response.tracks.items;
+        var playlist = $('<iframe>');
 
-        for (i = 0; i < tracksArray.length; i++) {
-          var track = $('<p>');
+        playlist.attr(
+          'src',
+          'https://open.spotify.com/embed?uri=spotify%3Auser%3Aspotify%3Aplaylist%3A2PXdUld4Ueio2pHcB6sM8j'
+        );
 
-          var currentTrack = tracksArray[i];
+        playlist.attr('width', '300');
 
-          // title
-          track.append('Song: ' + currentTrack.track.name + ' ');
+        playlist.attr('height', '380');
 
-          // artist
-          track.append(
-            '<br>' + 'Artist: ' + currentTrack.track.artists[0].name + ' '
-          );
+        playlist.attr('frameborder', '0');
 
-          // album
-          track.append(
-            '<br>' + 'Album: ' + currentTrack.track.album.name + ' '
-          );
+        playlist.attr('allowtransparency', 'true');
 
-          // length
-          trackTime = currentTrack.duration_ms;
-
-          track.append('<br>' + 'Length: ' + trackTime);
-
-          $('#playlist-display').append(track);
-        }
+        $('#playlist-display').append(playlist);
       });
     }
   }
@@ -139,24 +103,7 @@ $(document).ready(function() {
     event.preventDefault();
     displayPlaylist();
   });
+
+  //When the page load, execute the authorizeApp function
+  prepAuthorize();
 });
-
-// play
-// }
-
-// // on weather section load
-// $('#weather-display').hide();
-
-// $('#playlist-display').hide();
-
-// // $('#submit').css('margin', '30px auto')
-
-// $('#submit').click(function() {
-//   $('#form').hide();
-
-//   $('#weather-display').fadeIn(1000);
-
-//   $('#playlist-display').fadeIn(1000);
-
-//   $('#submit').css('margin', '20px auto');
-// });
